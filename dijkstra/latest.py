317950637d4ac0e4129023c77e081fdb697af334
bbmanalo <bbmanalo@Benjamin.localdomain>
2020-02-29 04:25:20 +0800

    s

--- dijkstra/latest.py
+++ dijkstra/latest.py
@@ -46,5 +46,5 @@ graph = Graph([("a", "b", 7),  ("a", "c", 9),  ("a", "f", 14), ("b", "c", 10),
                ("b", "d", 15), ("c", "d", 11), ("c", "f", 2),  ("d", "e", 6),
                ("e", "f", 9)])
 qw=input("Source: ")
-er=input("Destination: ")
+er=input("Destination: .")
 pp(graph.dijkstra(qw, er))
