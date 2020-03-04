34a0f10aa9f9125deb1d14774e84c32cf967b89b
Josh Angelo Escalona <josh.escalona@eee.upd.edu.ph>
2020-03-04 19:05:48 +0800

    made dijkstra implementation faster

--- dijkstra/dijkstra-final.py
+++ dijkstra/dijkstra-final.py
@@ -25,20 +25,27 @@ def dijkstra(graph, start, goal):
     # set starting point distance to 0
     shortest_distance[start] = 0
 
-
     while unseenNodes:
         minNode = None
+
+        # check node in graph with shortest distance and start there
         for node in unseenNodes:
             if minNode is None:
                 minNode = node
             elif shortest_distance[node] < shortest_distance[minNode]:
                 minNode = node
 
+        # check if current shortest distance is greater than minNode + weight
         for childNode, weight in graph[minNode].items():
             if weight + shortest_distance[minNode] < shortest_distance[childNode]:
                 shortest_distance[childNode] = weight + shortest_distance[minNode]
                 predecessor[childNode] = minNode
+
         unseenNodes.pop(minNode)
+        # end loop if goal node is reached
+        # we dont need to loop through all vertices
+        if minNode == goal:
+            break
 
     currentNode = goal
     while currentNode != start:
@@ -58,5 +65,5 @@ def dijkstra(graph, start, goal):
 # store loaded object to variable
 adj_list = load_object("adj_list_obj.pkl")
 
-graph = {'a':{'b':10,'c':3},'b':{'c':1,'d':2},'c':{'b':4,'d':8,'e':2},'d':{'e':7},'e':{'d':9}}
-dijkstra(adj_list, '249247443', '2672531535')
+# graph = {'a':{'b':10,'c':3},'b':{'c':1,'d':2},'c':{'b':4,'d':8,'e':2},'d':{'e':7},'e':{'d':9}}
+dijkstra(adj_list, '17216409', '5369579207')
